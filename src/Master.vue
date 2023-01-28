ea5610f49bb308ef962c230bde9d482c0c287898
Amir <amirhassan.hamzan@gmail.com>
2023-01-28 12:16:48 +0800

    Add vue@3, bootstrap 5, setup composition API

---
+++ src/Master.vue
@@ -0,0 +1,28 @@
+<template>
+  <link
+    rel="stylesheet"
+    href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
+  />
+  <div class="container mt-5 text-center">
+    <div class="row mx-auto">
+      <div class="col">
+        <div class="card">
+          <div class="card-body">
+            <div id="app">{{ uName }}</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script setup>
+import { ref } from "vue";
+
+const uName = ref("Amir");
+
+setTimeout(function () {
+  uName.value = "Hassan";
+}, 2000);
+</script>
---
+++ src/main.js
@@ -0,0 +1,7 @@
+import { createApp } from 'vue'
+// import App from './App.vue'
+// import './assets/main.css'
+
+import Master from './Master.vue'
+
+createApp(Master).mount('#app')
