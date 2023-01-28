6e2a4db0394732a378a89d0d335efa66d27d6700
Amir <amirhassan.hamzan@gmail.com>
2023-01-28 19:55:40 +0800

    Add computed property

--- src/Master.vue
+++ src/Master.vue
@@ -1,8 +1,5 @@
 <template>
-  <link
-    rel="stylesheet"
-    href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
-  />
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" />
   <div class="container mt-5 text-center">
     <div class="row mx-auto">
       <div class="col">
@@ -10,29 +7,36 @@
           <div class="card-body">
             <div id="app">
               <div>
-                {{ uName }}
+                Name: {{ uName }}
               </div>
               <div>
-                {{ uAge }}
+                Age: {{ uAge }}
               </div>
               <div>
-                {{ myMeal.theMeal }}
+                Meal: {{ myMeal.theMeal }}
               </div>
               <div>
                 <button @click="setNewAge()" class="btn m-1 btn-primary">
-                  Change Age
+                  Add Age
                 </button>
               </div>
               <div>
                 <button @click="addCheeseToMeal()" class="btn m-1 btn-warning">
                   Add Cheese
                 </button>
-                <button
-                  @click="removeCheeseFromMeal()"
-                  class="btn m-1 btn-outline-danger"
-                >
+                <button @click="removeCheeseFromMeal()" class="btn m-1 btn-outline-danger">
                   Remove Cheese
                 </button>
+              </div>
+              <div></div>
+              <div class="row mt-5">
+                <div class="mb-3">{{ fullName }}</div>
+                <div class="col">
+                  <input type="text" class="form-control" placeholder="First Name" @input="setFirstName">
+                </div>
+                <div class="col">
+                  <input type="text" class="form-control" placeholder="Last Name" @input="setLastName">
+                </div>
               </div>
             </div>
           </div>
@@ -46,19 +50,22 @@
 <script setup>
 // using ref function for string, int
 // using reactive function for object
-import { ref, reactive } from "vue";
+import { ref, reactive, computed } from "vue";
 
 const uName = ref("Amir");
 const uAge = ref(25);
 const myMeal = reactive({
   theMeal: "Pizza",
 });
+const firstName = ref("");
+const lastName = ref("");
 
 setTimeout(function () {
   uName.value = "Hassan";
   uAge.value = 99;
   myMeal.theMeal = "Burger";
 }, 2000);
+
 
 function setNewAge() {
   uAge.value++;
@@ -72,6 +79,19 @@ function addCheeseToMeal() {
 function removeCheeseFromMeal() {
   myMeal.theMeal = myMeal.theMeal.slice(0, -7);
 }
+
+function setFirstName(event) {
+  firstName.value = event.target.value;
+}
+
+function setLastName(event) {
+  lastName.value = event.target.value;
+}
+
+// computed property is readonly reactive ref object
+const fullName = computed(function () {
+  return firstName.value + " " + lastName.value;
+})
 
 // use composition API with basic syntax script
 // import { ref, reactive } from "vue";
